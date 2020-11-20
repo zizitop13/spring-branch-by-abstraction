4e88d4ffc5229e321bae9997a616a0893195b70f
17110774 <zinyakov.m.v@sberbank.ru>
2020-11-20 20:27:44 +0300

    initial commit

---
+++ src/main/java/ru/zizitop/demo/SpringBranchByAbstractionApplication.java
@@ -0,0 +1,13 @@
+package ru.zizitop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBranchByAbstractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBranchByAbstractionApplication.class, args);
+	}
+
+}
