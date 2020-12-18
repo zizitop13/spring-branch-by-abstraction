1f3475a01341963d96089e4d96b9d008a82faa1a
17110774 <zinyakov.m.v@sberbank.ru>
2020-12-18 09:28:53 +0300

    Extract abstraction

---
+++ src/main/java/ru/zizitop/demo/services/EmailNotificationService.java
@@ -0,0 +1,40 @@
+package ru.zizitop.demo.services;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+import ru.zizitop.demo.model.Notification;
+import ru.zizitop.demo.properties.FeatureProperties;
+import ru.zizitop.demo.properties.NotificationProperties;
+import ru.zizitop.demo.repositories.NotificationRepository;
+import ru.zizitop.demo.senders.EmailSender;
+
+@Service
+@RequiredArgsConstructor
+public class EmailNotificationService implements NotificationService {
+
+    private final EmailSender emailSender;
+
+    private final NotificationProperties notificationProperties;
+
+    private final FeatureProperties featureProperties;
+
+    @Nullable
+    private final NotificationRepository notificationRepository;
+
+
+    @Override
+    public void notify(Notification notification){
+        String from = notificationProperties.getSenderEmail();
+        String to = notification.getRecipient();
+        String subject = notificationProperties.getEmailSubject();
+        String text = notification.getText();
+        emailSender.sendEmail(from, to, subject, text);
+
+        if(featureProperties.isPersistence()){
+            notificationRepository.save(notification);
+        }
+    }
+
+}
--- src/main/java/ru/zizitop/demo/services/NotificationService.java
+++ src/main/java/ru/zizitop/demo/services/NotificationService.java
@@ -1,39 +1,7 @@
 package ru.zizitop.demo.services;
 
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Service;
 import ru.zizitop.demo.model.Notification;
-import ru.zizitop.demo.properties.FeatureProperties;
-import ru.zizitop.demo.properties.NotificationProperties;
-import ru.zizitop.demo.repositories.NotificationRepository;
-import ru.zizitop.demo.senders.EmailSender;
 
-@Service
-@RequiredArgsConstructor
-public class NotificationService {
-
-    private final EmailSender emailSender;
-
-    private final NotificationProperties notificationProperties;
-
-    private final FeatureProperties featureProperties;
-
-    @Nullable
-    private final NotificationRepository notificationRepository;
-
-
-    public void notify(Notification notification){
-        String from = notificationProperties.getSenderEmail();
-        String to = notification.getRecipient();
-        String subject = notificationProperties.getEmailSubject();
-        String text = notification.getText();
-        emailSender.sendEmail(from, to, subject, text);
-
-        if(featureProperties.isPersistence()){
-            notificationRepository.save(notification);
-        }
-    }
-
+public interface NotificationService {
+    void notify(Notification notification);
 }
